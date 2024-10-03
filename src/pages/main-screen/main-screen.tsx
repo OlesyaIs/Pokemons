d4a1b847d8c60b3581b773c86043a9706c88eb68
Olesia Isichenko <isichenko.ok@gmail.com>
2024-10-03 13:06:32 +0300

    feat: divide into pages

---
+++ src/pages/main-screen/main-screen.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import reactLogo from '../../assets/react.svg';
+import CardsList from '../../components/cards-list/cards-list';
+
+function MainScreen() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="page">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Pokemons</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <CardsList pokemonsData={[1, 2, 3]} />
+    </div>
+  );
+}
+
+export default MainScreen;
---
+++ src/pages/pokemon-screen/pokemon-screen.tsx
@@ -0,0 +1,11 @@
+import HomeButton from "../../components/home-button/home-button";
+
+function PokemonScreen() {
+  return (
+    <div className="page">
+      <HomeButton />
+    </div>
+  );
+}
+
+export default PokemonScreen;
