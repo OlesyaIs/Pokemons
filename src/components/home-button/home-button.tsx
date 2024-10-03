1672708bfc170f9c328784ffb0859ae676b7b436
Olesia Isichenko <isichenko.ok@gmail.com>
2024-10-03 13:39:28 +0300

    fix: improve home-button

--- src/components/home-button/home-button.tsx
+++ src/components/home-button/home-button.tsx
@@ -1,11 +1,12 @@
-import pokeball from '../../assets/pokeball.svg';
+import { NavLink } from "react-router-dom";
+import { AppRoute } from "../../const/const";
 
 function HomeButton() {
   return (
     <div>
-        <a href="#">
-          <img src={pokeball} className="logo" alt="pokeball" width={50} height={50}/>
-        </a>
+        <NavLink to={AppRoute.Main}>
+          <img src="/img/pokeball.svg" className="logo" alt="pokeball" width={70} height={70}/>
+        </NavLink>
     </div>
   );
 }
