c5840db92577f2cf8c4a99901083c359cbf85a0f
Olesia Isichenko <isichenko.ok@gmail.com>
2024-10-05 15:08:47 +0300

    feat: add styles
    Styles' refactoring, adding styles for light scheme and for mobile

--- src/components/header/home-button/home-button.tsx
+++ src/components/header/home-button/home-button.tsx
@@ -7,7 +7,7 @@ import { getPathWithBasePart } from '../../../utils/utils';
 function HomeButton() {
   return (
     <div className="home-button">
-        <Link to={AppRoute.Main}>
+        <Link to={AppRoute.Main} className='home-button__link'>
           <img src={getPathWithBasePart('/img/pokeball.svg')} className="home-button__logo" alt="pokeball" width={60} height={60}/>
           <span className="home-button__text">Pokémons</span>
         </Link>
